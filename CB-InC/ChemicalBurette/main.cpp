b809bf75a58fda8d7caca5bcf54bab372acadac7
boodah <boodah21@protonmail.com>
2019-04-23 00:04:41 -0500

    CB_InC - se inicia el desarrollo en C++

---
+++ CB-InC/ChemicalBurette/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "ui_main.h"
+
+Main::Main(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Main)
+{
+    ui->setupUi(this);
+}
+
+Main::~Main()
+{
+    delete ui;
+}
