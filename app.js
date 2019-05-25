5a574b12ef93d023759ea6e6cdca3b9a974f777c
boodah <boodah21@protonmail.com>
2019-05-24 23:37:42 -0500

    Se ajustan las dependencias.

--- app.js
+++ app.js
@@ -1,59 +1,80 @@
+'use strict'
+
+//Dependencias
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var app = express(); // inicializa la aplicacion expres
+//body parser
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//logger
+var logger = require('morgan');
+app.use(logger('dev'));
+//cookie / sessions
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-var app = express();
+//layout setup
+var exphbs = require('express-handlebars');
+var nib = require('nib');
+
+//Handelbars setups
+app.engine('nib', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + 'views/partials'
+
+}));
+
+// stylus setup
+var stylus = require('stylus');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Routes
+var routes = require('./routes/index');
+var users = require('./routes/users');
 app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: err
     });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
-module.exports = app;
+
+
+//inicializa el servidor
+if (!!module.parent)
+  module.exports = app;
+else
+  app.listen(3000);
