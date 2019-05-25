11b59d9284fc06ac6adda7ef5e31d99a4b79234a
Harry Alvarado <harry2alvarado8@gmail.com>
2019-05-24 23:16:38 -0500

    Se crean los componentes NoPageFound, Login y Register.

--- ChemicalBurette/src/app/app-routing.module.ts
+++ ChemicalBurette/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//
+// const appRoutes: Routes = [
+//
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { //LazyLoad
+//     path: '',
+//     component: PagesComponent,
+//     canActivate: [LoginGuardGuard],
+//     loadChildren: './pages/pages.module#PagesModule'
+//   },
+//   { path: '**', component: NopagefoundComponent },
+//
+// ];
+//
+// export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
--- ChemicalBurette/src/app/app.module.ts
+++ ChemicalBurette/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
