62f70cc69caae9a8a2cabef5f4fa4955c2ef06ca
boodah <boodah21@protonmail.com>
2019-07-05 13:44:13 -0500

    backendV1.8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var DATA = {
+  uid: "",
   codepc: "",
   email: "",
   lastname: "",
@@ -32,69 +33,43 @@ function main(DATA) {
 
 function firebaseConnect(admin, serviceAccount, config,firebase) {
   firebase.initializeApp(JSON.parse(config));
-  var database = firebase.database();
-  var hot_data_from_firebase = readUsersData(database);  //trae la data caliente de firebase
-  hot_data_from_firebase.then((resultado)=>{
-    var hot_data = [];
-    for(var i in resultado)
-      hot_data.push([i, resultado [i]]);
-    //console.log(hot_data[0][0]); esto trae los uids, etc
-    admin_init(admin,serviceAccount,hot_data); //ACTIVA EL ADMINISTRADOR PARA HACER LA CARGA Y EL ORDENAMIENTO
-  }).catch((error)=>{
-    console.log(error);
-  });
-} //fin firebaseconnect
-
-
-
-
-function admin_init(admin,serviceAccount,hot_data) {
   admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(serviceAccount)),
     databaseURL: "https://chemical-burette.firebaseio.com"
   });
-  CREA_USUARIOS_AUTENTIFICADOS(admin,hot_data);
-}
+  var database = firebase.database();
+  var schedule = require('node-schedule');
+  var hot_data_from_firebase="";
 
-//Este la idea es generalizar el inicio.
-function RegistraUsuarios(database, DATA) {
-  database
-    .ref(DATA.PATHHEAD + "/" + DATA.TIPO + "/" + DATA.Nombre_usuario)
-    .set({
-      ID: DATA.ID,
-      Email: DATA.emails,
-      Pass: DATA.pass,
-      CONFIG_MORE: {
-        Name: DATA.configMORE.Nombre,
-        Lastname: DATA.configMORE.Apellido,
-        CIP: DATA.configMORE.CIP,
-        Sex: DATA.configMORE.Genero,
-        ID_inst: DATA.configMORE.ID_INST,
-        ID_course: DATA.configMORE.ID_Materia,
-        DATE_START: DATA.configMORE.Fecha_START
-      }
+  var j = schedule.scheduleJob("*/2 * * * * *", function() {
+    hot_data_from_firebase = readUsersData(database);  //trae la data caliente de firebase
+    hot_data_from_firebase.then((resultado)=>{
+      var hot_data = [];
+      for(var i in resultado)
+        hot_data.push([i, resultado [i]]);
+      //console.log(hot_data[0][0]); //esto trae los uids, etc
+      CREA_USUARIOS_AUTENTIFICADOS(admin,hot_data,firebase);
+    }).catch((error)=>{
+      console.log("Sin Cambios, - NO DATA - ");
     });
-}
-
+  });
+} //fin firebaseconnect
 
 //Data del database procedente del registro
 function readUsersData(database) {
   return new Promise((resolve, reject)=>{
-    database.ref("students" + "/").on("value", snapshot => {
+    database.ref("students/").on("value", snapshot => {
       const result = snapshot.val();
+       console.log(result);
       if (result!= null)
-      return resolve(result);
+        return resolve(result);
       else
-      return reject(new Error("Conexion con fuente de dato desconocida ---NO DATA---\n\n\n."));
-      //console.log('Users_type -- ['+"DATA"+']:  ', result);
+        return reject(new Error("Conexion con fuente de dato desconocida ---NO DATA---\n\n\n."));
     });
   });
 }
+
 //Data del database procedente del registro
-
-
-
-
 
 function consulta_UID(uid, admin) {
   admin
@@ -108,9 +83,9 @@ function consulta_UID(uid, admin) {
     });
 }
 
-function CREA_USUARIOS_AUTENTIFICADOS(admin, hot_data) {
+function CREA_USUARIOS_AUTENTIFICADOS(admin, hot_data,firebase) {
   var db = admin.database();
- for (let i = 0; i < hot_data.length; i++) {
+ for (let i = 0; i < hot_data.length-1; i++) {
     admin
     .auth()
     .createUser({
@@ -122,25 +97,18 @@ function CREA_USUARIOS_AUTENTIFICADOS(admin, hot_data) {
       disabled: false
     })
     .then(function(userRecord) {
-      console.log("Successfully created new user:", userRecord.uid);
-        var ref = db.ref("students/"+userRecord.uid);
+          var ref = db.ref("students/"+userRecord.uid);
             ref.remove()
               .then(function() {
-                console.log("Remove succeeded from database RealTime.")
+                console.log("Successfully created new user:", userRecord.uid);
+                firebase.auth().signInWithEmailAndPassword(hot_data[i][1].email, hot_data[i][1].password).catch(function(error) {console.log(error.message)});
               })
               .catch(function(error) {
                 console.log("Remove failed: " + error.message)
               });
     })
     .catch(function(error) {
-      console.log("Error al crear nuevos usuarios:", error);
+      console.log("ERROR: "+error.message);
     });
   }
 }//end
-
-function md5(string) {
-  return crypto
-    .createHash("md5")
-    .update(string)
-    .digest("hex");
-}
